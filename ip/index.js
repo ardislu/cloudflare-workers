22d9be3d199cf28dff4e3381d80a4b5ee899793a
Ardis Lu <ardisglu@gmail.com>
2022-07-10 21:58:09 -0700

    Fix regression in IP worker
    New module syntax directly passes the request instead of the fetch event

--- ip/index.js
+++ ip/index.js
@@ -1,5 +1,5 @@
 export default {
   async fetch(request) {
-    return new Response(event.request.headers.get('CF-Connecting-IP'));
+    return new Response(request.headers.get('CF-Connecting-IP'));
   }
 }
