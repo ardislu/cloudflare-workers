70f2e429346e2299fe13276f027976dc96a06e11
Ardis Lu <ardisglu@gmail.com>
2023-12-24 18:40:25 -0800

    Pin eruda version to 3.0.1

--- eruda/index.js
+++ eruda/index.js
@@ -2,7 +2,7 @@ import { SetBase, UpdateLink } from '../proxy';
 
 class InjectEruda {
   element(element) {
-    element.prepend('<script src="//cdn.jsdelivr.net/npm/eruda"></script><script>eruda.init();</script>', { html: true });
+    element.prepend('<script src="//cdn.jsdelivr.net/npm/eruda@3.0.1/eruda.js" integrity="sha512-iqTxJJyPkonhuLaymGBUWM6x/icdahslHnzOZjrQifaSH+RjA3SGNGhKWboNqhbekZiN63UeorCfIz6XJBD/Fw==" crossorigin></script><script>eruda.init();</script>', { html: true });
   }
 }
 
